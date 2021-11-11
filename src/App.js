2586b8dafcbd49b53a606576ec8c7d7507970887
VadDoc <vadprudnikov@gmail.com>
2021-11-11 10:07:16 +0200

    add project

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import {Header} from "./Components/Header/Header";
+import {Main} from "./Components/Main/Main";
+import {Footer} from "./Components/Footer/Footer";
+import {BrowserRouter} from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Header/>
+        <Main/>
+        <Footer/>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
