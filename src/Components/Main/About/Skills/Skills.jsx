e2285fc5d94cb4abe85d2f6d27c95ab3b8959410
VadDoc <vadprudnikov@gmail.com>
2021-11-19 10:10:37 +0200

    new

--- src/Components/Main/About/Skills/Skills.jsx
+++ src/Components/Main/About/Skills/Skills.jsx
@@ -1,12 +1,37 @@
 import styles from "./Skills.module.css";
 
 const mySkills = [
-  {name: 'React', per: 20},
-  {name: 'Redux', per: 80},
-  {name: 'JavaScript', per: 70},
-  {name: 'Wordpress', per: 50},
-  {name: 'CSS', per: 10},
-  {name: 'HTML', per: 100},
+  {name: 'React', per: 67},
+  {name: 'Redux', per: 82},
+  {name: 'TypeScript', per: 62},
+  {name: 'JavaScript', per: 55},
+  {name: 'Axios', per: 65},
+  {name: 'Storybook', per: 55},
+  {name: 'Unit Testing', per: 45},
+  // {name: 'HTML', per: 100},
+  {name: 'Material UI', per: 48},
+
+  {name: 'AJAX', per: 68},
+  {name: 'Git', per: 45},
+  {name: 'JSON', per: 73},
+  {name: 'NODEJS', per: 35},
+  // {name: 'HTML', per: 100},
+  // {name: 'CSS', per: 10},
+  // {name: 'HTML', per: 100},
+  
+  {name: 'Wordpress', per: 51},
+  {name: 'SASS', per: 58},
+  {name: 'LESS', per: 55},
+  {name: 'SCSS', per: 69},
+  {name: 'Bootstrap', per: 72},
+  {name: 'CSS', per: 95},
+  {name: 'HTML', per: 99},
+
+
+  {name: 'MySQL', per: 23},
+  {name: 'Photoshop', per: 45},
+  // {name: 'CSS', per: 95},
+  // {name: 'HTML', per: 99},
 ]
 
 export const Skills = ({skill}) => {
