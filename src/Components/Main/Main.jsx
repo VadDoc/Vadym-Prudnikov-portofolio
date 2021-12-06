7ce3e6b9a34ca8a10e021de4a5359c3146a7a1fa
VadDoc <vadprudnikov@gmail.com>
2021-12-06 08:42:44 +0200

    redirect

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,5 +1,5 @@
 import styles from './Main.module.scss'
-import {Route} from "react-router-dom";
+import {Redirect, Route} from "react-router-dom";
 import {Home} from "./Home/Home";
 import {About} from "./About/About";
 import {Projects} from "./Projects/Projects";
@@ -8,10 +8,12 @@ import {Contacts} from "./Contacts/Contacts";
 export const Main = () => {
   return (
       <div className={styles.main}>
-        <Route path='/home' render={() => <Home name="Home" />} />
+        <Route path='/' render={() => <Home name="Home" />} />
+        <Route path='/home' render={() => <Home name="Home" />
         <Route path='/about' render={() => <About name="About" />} />
         <Route path='/projects' render={() => <Projects name="Projects" />} />
         <Route path='/contacts' render={() => <Contacts name="Contacts" />} />
+        {/*<Redirect from='/' to='/home' />*/}
       </div>
   )
 }
