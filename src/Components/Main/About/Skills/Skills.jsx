f66eac03130ab5ec242b33a025a653676372c8c1
VadDoc <vadprudnikov@gmail.com>
2021-12-06 12:20:05 +0200

    info

--- src/Components/Main/About/Skills/Skills.jsx
+++ src/Components/Main/About/Skills/Skills.jsx
@@ -5,7 +5,11 @@ export const Skills = () => {
   return (
     <div className={styles.skills}>
       <h3 className={styles.title}>My skills</h3>
-      <div className={styles.allSkills}><span>I use in my practice following languages, skills and technologies: {about.allSkills}</span></div>
+      <div className={styles.allSkills}>
+        <p>I use in my practice following languages, skills and technologies: <span>{about.allSkills}</span></p>
+        <p>My next goals are to improve in React and related technologies, as well as deep learning Angular. Working hard to improve my English.</p>
+        <p>In the future I see myself as a Full Stack Developer.</p>
+        </div>
 
       {about.mySkills.map(sk => (
         <div className={styles.skill} key={sk.id}>
