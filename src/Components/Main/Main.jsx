6cab0ecb93333d7856b6060e3936288a31b8d3f9
VadDoc <vadprudnikov@gmail.com>
2021-12-06 08:44:56 +0200

    fix

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,5 +1,5 @@
 import styles from './Main.module.scss'
-import {Redirect, Route} from "react-router-dom";
+import {Route} from "react-router-dom";
 import {Home} from "./Home/Home";
 import {About} from "./About/About";
 import {Projects} from "./Projects/Projects";
@@ -9,11 +9,10 @@ export const Main = () => {
   return (
       <div className={styles.main}>
         <Route path='/' render={() => <Home name="Home" />} />
-        <Route path='/home' render={() => <Home name="Home" />
+        <Route path='/home' render={() => <Home name="Home" />} />
         <Route path='/about' render={() => <About name="About" />} />
         <Route path='/projects' render={() => <Projects name="Projects" />} />
         <Route path='/contacts' render={() => <Contacts name="Contacts" />} />
-        {/*<Redirect from='/' to='/home' />*/}
       </div>
   )
 }
